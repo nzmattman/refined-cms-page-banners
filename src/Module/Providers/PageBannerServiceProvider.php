c02b0e348c905f1e49ca7ac20c804e8e88ea8ac3
Matthias Law <matthias@refineddigital.co.nz>
2018-08-21 01:16:33 +0000

    adding extra fields to config

--- src/Module/Providers/PageBannerServiceProvider.php
+++ src/Module/Providers/PageBannerServiceProvider.php
@@ -29,14 +29,20 @@ class PageBannerServiceProvider extends ServiceProvider
 
         $this->mergeConfigFrom(__DIR__.'/../../../config/page-banners.php', 'page-banners');
 
+        $fields = [
+            [ 'name' => 'Image', 'page_content_type_id' => 4, 'field' => 'image', 'hide_label' => true ]
+        ];
+
+        if (config('page-banners.extra_fields')) {
+            $fields = array_merge($fields, config('page-banners.extra_fields'));
+        }
+
         app(PageAggregate::class)
             ->addModule('Banners', [
                 'tab' => 'banners',
                 'type' => 'repeatable',
                 'config' => 'page-banners',
-                'fields' => [
-                    [ 'name' => 'Image', 'page_content_type_id' => 4, 'field' => 'image' ]
-                ]
+                'fields' => $fields
             ]);
     }
 }
